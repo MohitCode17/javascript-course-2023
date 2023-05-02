baa2a25bad87246015a04a61ed2b8c4a20d9b985
MohitGupta <krishgupta1630@gmail.com>
2023-05-02 22:55:46 +0530

    changes in lecture 06

--- Lecture06/app.js
+++ Lecture06/app.js
@@ -182,6 +182,26 @@ switch (expression) {
 
 
 
+// 👉 TERNARY OPERATOR
+
+// The conditional (ternary) operator is the only JavaScript operator that takes three operands: a condition followed by a question mark (?), then an expression to execute if the condition is truthy followed by a colon (:), and finally the expression to execute if the condition is falsy. This operator is frequently used as an alternative to an if...else statement.
+
+
+// ➡️ Example
+
+// let isRaining = true
+// isRaining
+//   ? console.log('You need a rain coat.')
+//   : console.log('No need for a rain coat.')
+
+
+// isRaining = false
+// isRaining
+//   ? console.log('You need a rain coat.')
+//   : console.log('No need for a rain coat.')
+
+
+
 // 👉 BREAK AND CONTINUE STATEMENT
 
 // Break is used to interrupt a loop.
