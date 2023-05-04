34a71cf629607f3dd7fefc82cef161b935624d05
MohitGupta <krishgupta1630@gmail.com>
2023-05-04 21:49:42 +0530

    Added lecture07

--- Lecture07/app.js
+++ Lecture07/app.js
@@ -75,18 +75,6 @@ Function makes code clean, easy to read, reusable and easy to test.
 // console.log(square(2));
 
 
-// const changeToUpperCase = arr => {
-//     const newArr = []
-//     for (const element of arr) {
-//         newArr.push(element.toUpperCase())
-//     }
-//     return newArr
-// }
-
-// const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
-// console.log(changeToUpperCase(countries))
-
-
 // 👉 PASSING FUNCTION AS AN ARGUMENT (HIGHER ORDER FUNCTION)
 
 let upperCase = function(str){
