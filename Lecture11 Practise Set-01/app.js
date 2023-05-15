c2fbc2ba3658b989b327931786ebc3f0b837d942
MohitGupta <krishgupta1630@gmail.com>
2023-05-15 22:38:38 +0530

    Add Practise Set-01

--- Lecture11 Practise Set-01/app.js
+++ Lecture11 Practise Set-01/app.js
@@ -111,7 +111,8 @@ Q.24 An application created using MongoDB, Express, React and Node is called a M
 Q.25 The following is an array of 10 students ages:
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 
-- Sort the array and find the min and max age
+Sort the array and find the min and max age:-
+
 Find the median age(one middle item or two middle items divided by two)
 Find the average age(all items divided by number of items)
 Find the range of the ages(max minus min)
