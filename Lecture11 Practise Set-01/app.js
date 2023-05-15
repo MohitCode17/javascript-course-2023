1c78358b14a45822852f8c4f162e952884ab64f6
MohitGupta <krishgupta1630@gmail.com>
2023-05-15 22:31:27 +0530

    Add Practise Set-01

---
+++ Lecture11 Practise Set-01/app.js
@@ -0,0 +1,120 @@
+/*
+💯 PRACTISE SET 01 QUESTIONS
+
+Q.1 Declare a variable named challenge and assign it to an initial value 'JavaScript is most popular programming language' and change all the characters to capital letters.
+
+Q.2 Cut out the first word, which declare in challenge variable and print it in console window.
+
+Q.3 Check if the string 'JavaScript is most popular programming language' contains a word 'popular'. If not print false else true.
+
+Q.4 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+Q.5 Check if string 'JavaScript is most popular programming language' start with 'JavaScript' if not print false else true.
+
+Q.6 Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+
+Q.7 Generate a random string by access "JavaScript".
+
+Q.8 Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+Enter your age: 30
+You are old enough to drive.
+Enter your age:15
+You are left with 3 years to drive.
+
+Q.9 Even numbers are divisible by 2 and the remainder is zero. Write a function to check, if a number is even or not.
+
+Q.10 Write a code which can give grades to students according to theirs scores:
+
+80-100, A
+70-89, B
+60-69, C
+50-59, D
+0-49, F
+
+Q.11 Write a program which tells the number of days in a month.
+Enter a month: January
+January has 31 days.
+
+Enter a month: JANUARY
+January has 31 day
+
+Enter a month: February
+February has 28 days.
+
+Q.12 Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon. Print the first company, middle and last company
+
+Q.13 Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+
+Q.14 Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+
+Q.15 Filter out companies which have more than one 'o' without the filter method and store it in new array.
+
+Q.16 In the following shopping cart add, remove, edit items.
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+
+1. add 'Meat' in the beginning of your shopping cart if it has not been already added
+2. add Sugar at the end of you shopping cart if it has not been already added
+3. modify Tea to 'Green Tea'
+4. remove 'Honey' if you are allergic to honey
+
+Q.17 const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+]
+
+In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+Q.18 Write a loop that makes the following pattern using console.log():
+#
+##
+###
+####
+#####
+######
+#######
+
+Q.19 Using loop print the following pattern:-
+
+i    i^2   i^3
+0    0     0
+1    1     1
+2    4     8
+3    9     27
+4    16    64
+5    25    125
+6    36    216
+7    49    343
+8    64    512
+9    81    729
+10   100   1000
+
+Q.20 Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+
+Q.21 Develop a function which generate array of 5 random numbers and the numbers must be unique.
+
+Q.22 Develop a function which generate a six characters random id:
+
+Q.23 Write a function which generates a random rgb color number.
+
+Q.24 An application created using MongoDB, Express, React and Node is called a MERN stack app. Create the acronym MERN by using the array mernStack.
+
+Q.25 The following is an array of 10 students ages:
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+- Sort the array and find the min and max age
+Find the median age(one middle item or two middle items divided by two)
+Find the average age(all items divided by number of items)
+Find the range of the ages(max minus min)
+
+
+*/
